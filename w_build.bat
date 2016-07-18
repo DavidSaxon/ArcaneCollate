f3e50142c4c592c6842f7a89acb34d9fa5da2add
David Saxon <davidesaxon@gmail.com>
2016-07-19 01:23:03 +1200

    setup for windows

---
+++ w_build.bat
@@ -0,0 +1,4 @@
+set VCTargetsPath=c:\Program Files (x86)\MSBuild\Microsoft.Cpp\v4.0\V120
+
+msbuild ..\ArcaneCollate.sln /v:n /p:Configuration=collate_lib
+msbuild ..\ArcaneCollate.sln /v:n /p:Configuration=tests
