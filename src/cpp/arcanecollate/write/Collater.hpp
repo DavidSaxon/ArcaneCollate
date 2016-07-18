719e3360381b62c4f647a0b2b76aca7b425637e5
David Saxon <davidesaxon@gmail.com>
2016-07-19 00:02:02 +1200

    added icon to docs

--- src/cpp/arcanecollate/write/Collater.hpp
+++ src/cpp/arcanecollate/write/Collater.hpp
@@ -5,7 +5,7 @@
 #ifndef ARCANECOLLATE_WRITE_COLLATER_HPP_
 #define ARCANECOLLATE_WRITE_COLLATER_HPP_
 
-#include <arcanecore/base/str/UTF8String.hpp>
+#include <arcanecore/io/sys/Path.hpp>
 
 namespace arccol
 {
@@ -32,6 +32,26 @@ public:
 
     // TODO:
     Collater();
+
+    //--------------------------------------------------------------------------
+    //                          PUBLIC MEMBER FUNCTIONS
+    //--------------------------------------------------------------------------
+
+    /*!
+     * \brief Sets the path this Collator will write its table of contents to.
+     *
+     * \param path The path that the table of contents will be written to.
+     * \param append Whether this table of contents will be appended to the
+     *               given file or override it.
+     * \param hash Whether the table of contents will be hashed to make it
+     *             non-human readable.
+     */
+    // void set_table_of_contents(
+    //         const arc::io::sys::Path& path,
+    //         bool append = false,
+    //         bool hash = false);
+
+    // void add_file();
 };
 
 } // namespace write
